b6a39415206a7787ac6561272ce100e836d8f0c6
Do Nghia <dothenry1607@gmail.com>
2025-10-21 23:04:29 -0700

    Refactor play_music function to Play_Music

--- soundtrack.py
+++ soundtrack.py
@@ -1,12 +1,11 @@
 from pygame.mixer import music, init
 from pygame.time import Clock 
-from pygame.event import poll
 init()
 
 
 
     
-def play_music(song : str , wait : bool = False, loop : bool = False):
+def Play_Music(song : str , wait : bool = False, loop : bool = False):
     music.load(song)
     if loop == True:
         music.play(-1)
